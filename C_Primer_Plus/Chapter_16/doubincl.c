d8b055f992f53ed4f6ceddf23ef3fc877acb61ba
alanMK <alanmusk@163.com>
2023-02-03 23:50:06 +0800

    Chapter_16

---
+++ C_Primer_Plus/Chapter_16/doubincl.c
@@ -0,0 +1,11 @@
+// doubincl.c -- 包含头文件两次
+#include <stdio.h>
+#include "names.h"
+#include "names.h"
+
+int main(void)
+{
+    names winner={"Less","Ismoor"};
+    printf("The winner is %s %s.\n",winner.first,winner.last);
+    return 0;
+}
