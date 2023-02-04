08039ecb3cb91f887e22bcf376c16d13dfca8f9a
alanMK <alanmusk@163.com>
2023-02-04 23:22:48 +0800

    Chapter_17

---
+++ C_Primer_Plus/Chapter_17/films2.c
@@ -0,0 +1,91 @@
+// films2.c -- 使用结构链表
+#include <stdio.h>
+#include <stdlib.h>     //提供malloc()原型
+#include <string.h>     //提供strcpy()原型
+#define TSIZE 45         //存储片名的数组大小
+
+struct film{
+    char title[TSIZE];
+    int rating;
+    struct film * next; //指向链表中的下一个结构
+};
+char * s_gets(char * st,int n);
+
+int main(void)
+{
+    struct film * head=NULL;
+    struct film * prev,* current;
+    char input[TSIZE];
+    //收集并存储信息
+    puts("Enter first movie title:");
+    while(s_gets(input,TSIZE)!=NULL&&input[0]!='\0')
+    {
+        current=(struct film *)malloc(sizeof(struct film));
+        if(head==NULL)//第一个结构
+        {
+            head=current;;
+        }
+        else
+        {
+            prev->next=current;
+        }
+        current->next=NULL;
+        strcpy(current->title,input);
+        puts("Enter your rating <0-10>:");
+        scanf("%d",&current->rating);
+        while(getchar()!='\0')
+        {
+            continue;
+        }
+        puts("Enter next movie title(empty line to stop):");
+        prev=current;
+    }
+    //显示电影列表
+    if(head==NULL)
+    {
+        printf("No data entered.");
+    }
+    else
+    {
+        printf("Here is the movie list:\n");
+    }
+    current=head;
+    while(current!=NULL)
+    {
+        printf("Moive:%s Rating:%d\n",current->title,current->rating);
+        current=current->next;
+    }
+    //完成任务,释放已分配的内存
+    current=head;
+    while(current!=NULL)
+    {
+        head=current->next;
+        free(current);
+        current=head;
+    }
+    printf("Bye!\n");
+    return 0;
+}
+
+char * s_gets(char * st,int n)
+{
+    char * ret_val;
+    char * find;
+    ret_val=fgets(st,n,stdin);
+    if(ret_val)
+    {
+        find=strchr(st,'\n');
+        if(find)
+        {
+            *find='\0';
+        }
+        else
+        {
+            while(getchar()!='\n')
+            {
+                continue;
+            }
+        }
+    }
+    return ret_val;
+}
---
+++ C_Primer_Plus/Chapter_17/list.c
@@ -0,0 +1,115 @@
+// list.c -- 支持链表操作的函数
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+//局部函数原型
+static void CopyToNode(Item item,Node * pnode);
+
+//接口函数
+//把链表设置为空
+void InitializeList(List * plist)
+{
+    *plist =NULL;
+}
+
+//如果链表为空,返回true
+bool ListIsEmpty(const List * plist)
+{
+    if(*plist==NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//如果链表已满,返回true
+bool ListIsFull(const List * plist)
+{
+    Node * pt;
+    bool full;
+
+    pt=(Node *)malloc(sizeof(Node));
+    if(pt==NULL)
+    {
+        full=true;
+    }
+    else
+    {
+        full=false;
+    }
+    free(pt);
+    return full;
+}
+
+//返回节点的数量
+unsigned int ListItemCount(const List * plist)
+{
+    unsigned int count=0;
+    Node * pnode =*plist;   //设置链表的开始
+    while(pnode!=NULL)
+    {
+        ++count;
+        pnode=pnode->next;
+    }
+    return count;
+}
+
+//创建存储项的节点,并将其添加至由plist指向的链表末尾(缓慢的实现)
+bool AddItem(Item item,List * plist)
+{
+    Node * pnew;
+    Node * scan=*plist;
+
+    pnew=(Node *)malloc(sizeof(Node));
+    if(pnew==NULL)
+    {
+        return false;   //失败时退出函数
+    }
+    CopyToNode(item,pnew);
+    pnew->next=NULL;
+    if(scan==NULL)  //如果是空链表,把pnew放在链表的开头
+    {
+        *plist=pnew;
+    }
+    else
+    {
+        while(scan->next!=NULL)
+        {
+            scan=scan->next;//找到链表的末尾
+        }
+        scan->next=pnew;
+    }
+    return true;
+}
+//访问每个节点并执行pfun指向的函数
+void Traverse(const List * plist,void (*pfun)(Item item))
+{
+    Node * pnode=*plist;    //设置链表的开始(链表开始作为锚点,不能修改)
+    while(pnode!=NULL)
+    {
+        (*pfun)(pnode->item);   //调用函数
+        pnode=pnode->next;      //前进到下一项
+    }
+}
+//释放由malloc()分配的内存
+//设置链表指针为NULL
+void EmptyTheList(List * plist)
+{
+    Node * psave;
+    while(*plist !=NULL)
+    {
+        psave=(*plist)->next;
+        free(*plist);
+        *plist=psave;
+    }
+}
+//局部函数定义
+//把一个项拷贝到节点中
+static void CopyToNode(Item item,Node * pnode)
+{
+    pnode->item=item;
+}
