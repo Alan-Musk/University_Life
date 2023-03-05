88ac112d63308827fa3f7e3ad42190d656578551
alanMK <alanmusk@163.com>
2023-03-05 23:06:45 +0800

    Python

--- Intro_to_Programming_in_Python/Chapter2/2.2/example_04.py
+++ Intro_to_Programming_in_Python/Chapter2/2.2/example_04.py
@@ -5,13 +5,13 @@ import stdrandom
 
 n=int(sys.argv[1])
 probabilities=stdarray.readFloat1D()
-cx=stdarray.readFloat2D
-cy=stdarray.readFloat2D
+cx=stdarray.readFloat2D()
+cy=stdarray.readFloat2D()
 x=0.0
 y=0.0
 stddraw.setPenRadius(0.0)
-stddraw.setXscale(-3.0,3.0)
-stddraw.setYscale(-3.0,3.0)
+stddraw.setXscale(0.0,1.0)
+stddraw.setYscale(0.0,1.0)
 for i in range(n):
     r=stdrandom.discrete(probabilities)
     x0=cx[r][0]*x+cx[r][1]*y+cx[r][2]
@@ -19,4 +19,4 @@ for i in range(n):
     x=x0
     y=y0
     stddraw.point(x,y)
-    stddraw.show(50)
+stddraw.show(10000)
