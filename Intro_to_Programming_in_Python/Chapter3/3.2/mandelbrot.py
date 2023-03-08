a23426899ac5a5ec0d8dbd8dc42095b2a1e58c81
alanMK <alanmusk@163.com>
2023-03-08 22:31:23 +0800

    Python

--- Intro_to_Programming_in_Python/Chapter3/3.2/mandelbrot.py
+++ Intro_to_Programming_in_Python/Chapter3/3.2/mandelbrot.py
@@ -2,20 +2,21 @@ import sys
 import stddraw
 from color import Color
 from picture import Picture
-import stdio
 
+# return 灰度
 def mandel(z0,limit):
     z=z0
     for i in range(limit):
         if abs(z)>2.0:  return i
         z=z*z+z0
     return limit
-n=int(sys.argv[1])
-xc=float(sys.argv[2])
-yc=float(sys.argv[3])
-size=float(sys.argv[4])
-
+n=int(sys.argv[1]) # n*n像素图像
+xc=float(sys.argv[2])   # 图像中心点
+yc=float(sys.argv[3])   # 图像中心点
+size=float(sys.argv[4]) # 图像大小
+# 创建图像对象
 pic=Picture(n,n)
+# 遍历图像
 for col in range(n):
     for row in range(n):
         x0=xc-size/2+size*col/n
