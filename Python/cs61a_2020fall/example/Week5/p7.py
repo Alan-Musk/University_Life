d2d2833813eb714f0cfd5374570c6b401165adb6
alanMK <alanmusk@163.com>
2023-07-16 20:45:34 +0800

    P

---
+++ Python/cs61a_2020fall/example/Week5/p7.py
@@ -0,0 +1,6 @@
+# Lecture 12: Tree Q & A 
+# 1.Can a tree have any number of branches more than two?
+# 2.About the optional second argument in the build-in sum function
+# 3.Trees: What happens if you ask for the branches of a leaf
+# 4. Tree processing which is confusing and whether we could do an example that doesn't involve leafs and branches
+# 5. What guarantees do you have with the tree data abstraction like how do all the pieces fit together
