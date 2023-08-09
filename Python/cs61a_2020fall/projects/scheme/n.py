6eaa49da25f6f04863f70fbbbcfc33e9253ab88b
alanMK <alanmusk@163.com>
2023-08-09 22:02:23 +0800

    HW08

--- Python/cs61a_2020fall/projects/scheme/n.py
+++ Python/cs61a_2020fall/projects/scheme/n.py
@@ -1,6 +1,7 @@
 with open("n.txt","r") as input_file:
     text=input_file.read()
     text=text.replace(",","\n").replace(".","\n")
+    
 
 with open("out.txt","w") as output_file:
     output_file.write(text)
