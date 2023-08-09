48525d728e751a09a38ae184fdde662966b244f5
alanMK <alanmusk@163.com>
2023-08-09 11:13:50 +0800

    Project 4

--- Python/cs61a_2020fall/projects/scheme/questions.scm
+++ Python/cs61a_2020fall/projects/scheme/questions.scm
@@ -13,20 +13,30 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (enumerate-form n reminder-s)
+      (if (null? reminder-s)
+         '()
+          (cons (list n (car reminder-s)) (enumerate-form (+ n 1) (cdr reminder-s)))
+    ))
+    (enumerate-form 0 s)
   )
   ; END PROBLEM 15
-
+(enumerate '(3 4 5 6))
+;((0 3) (1 4) (2 5) (3 6))
 ;; Problem 16
 
 ;; Merge two lists LIST1 and LIST2 according to COMP and return
 ;; the merged lists.
 (define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond 
+        ((null? list1) list2)
+        ((null? list2) list1)
+        ((comp (car list1) (car list2)) (cons (car list1) (merge comp (cdr list1) list2)))
+        (else (cons (car list2) (merge comp list1 (cdr list2))))
+    )
   )
   ; END PROBLEM 16
-
 
 (merge < '(1 5 7 9) '(4 8 10))
 ; expect (1 4 5 7 8 9 10)
@@ -36,11 +46,23 @@
 ;; Problem 17
 
 (define (nondecreaselist s)
-    ; BEGIN PROBLEM 17
-    'replace-this-line
-    )
+  ; BEGIN PROBLEM 17
+  (cond 
+    ((eq? nil s)
+     nil)
+    ((= 1 (length s))
+     (cons s nil))
+    ((> (car s) (cadr s))
+     (cons (cons (car s) nil)
+           (nondecreaselist (cdr s))))
+    (else
+     (cons
+      (cons (car s) (car (nondecreaselist (cdr s))))
+      (cdr (nondecreaselist (cdr s)))))
+  ))
     ; END PROBLEM 17
-
+    
+(nondecreaselist '(3 4 5 6))
 ;; Problem EC
 ;; Returns a function that checks if an expression is the special form FORM
 (define (check-special form)
